5ad9485d4355caaead0185bf82c72e4d7499f58a
Nurvandwi <44267095+nurvandwi@users.noreply.github.com>
2019-03-23 20:40:23 +0700

    exercise4

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('button').addEventListener("click", function() {
+	document.querySelector('.bg-model').style.display = "flex";
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.bg-model').style.display = "none";
+});
